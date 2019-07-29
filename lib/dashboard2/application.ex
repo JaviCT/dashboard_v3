f3c9edff397b660006bf76027bdc281d3c68c5d2
javict <6javierct93@gmail.com>
2019-07-29 12:09:19 +0100

    Removed repo

--- lib/dashboard2/application.ex
+++ lib/dashboard2/application.ex
@@ -8,8 +8,6 @@ defmodule Dashboard2.Application do
 
     # Define workers and child supervisors to be supervised
     children = [
-      # Start the Ecto repository
-      supervisor(Dashboard2.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Dashboard2Web.Endpoint, []),
       # Start your own worker by calling: Dashboard2.Worker.start_link(arg1, arg2, arg3)
--- mix.exs
+++ mix.exs
@@ -33,17 +33,12 @@ defmodule Dashboard2.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 4.0.0"},
-      {:ecto, "~> 3.0.6"},
-      {:ecto_sql, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:plug_cowboy, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
       {:evercam_models, github: "evercam/evercam_models"},
       {:jason, "~> 1.1"}
     ]
